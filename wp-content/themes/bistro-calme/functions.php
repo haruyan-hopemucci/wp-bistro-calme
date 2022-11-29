9cb1d7c582732fa2a1ecedf5320fdb53b58a39f1
haruyan-hopemucci <haruyanhpm@gmail.com>
2022-11-30 02:04:52 +0900

    feat: chapter2 section5

---
+++ wp-content/themes/bistro-calme/functions.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * <title>タグを出力する
+ */
+add_theme_support('title-tag');
+
+/** タイトル表示のセパレーター（titleとdescriptionの間）を変更する */
+add_filter('document_title_separator', 'my_document_title_separator');
+
+function my_document_title_separator($separator)
+{
+    $separator = "😄";  // 絵文字も行けるよ！
+    return $separator;
+}
+
+/** タイトルの文字列を任意の値に変更できる */
+add_filter('document_title_parts', 'my_document_title_parts');
+
+function my_document_title_parts($title)
+{
+    // homeだけタイトル表記を変えるようにできる
+    if (is_home()) {
+        unset($title['tagline']);
+        $title['title'] = 'BISTRO CALME は、カジュアルなワインバーよりなビストロです。';
+    }
+    return $title;
+}
--- wp-content/themes/bistro-calme/header.php
+++ wp-content/themes/bistro-calme/header.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?= bloginfo('name'); ?></title>
     <link href="<?= get_template_directory_uri(); ?>/assets/css/styles.min.css" rel="stylesheet">
     <?php
     // Font Awesomeを重複して読み込まないようにする
