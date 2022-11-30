ab483069ee66adf6768722cc3c1cf0b8af255075
haruyan-hopemucci <haruyanhpm@gmail.com>
2022-11-30 22:02:25 +0900

    chapter2 section6 画像以外の記事ループまで

--- wp-content/themes/bistro-calme/index.php
+++ wp-content/themes/bistro-calme/index.php
@@ -1,75 +1,40 @@
 <?php get_header(); ?>
 
-<div class="row">
-	<div class="col-md-4">
-		<article class="news">
-			<div class="news_pic">
-				<a href="#">
-					<img src="<?= get_template_directory_uri(); ?>/assets/img/home/news_img-1.jpg" alt="">
-				</a>
-			</div>
-			<div class="news_meta">
-				<ul class="post-categories">
-					<li><a href="#">お知らせ</a></li>
-				</ul>
-				<time class="news_time" datetime="2019-00-00">2019年00月00日</time>
-			</div>
-			<h2 class="news_title"><a href="#">タイトルタイトルタイトルタイトル</a></h2>
-			<div class="news_desc">
-				<p>概要が入ります。概要が入ります。概要が入ります。概要が入ります。概要が入ります。概要が入ります。</p>
-				<p><a href="#">[続きを読む]</a></p>
-			</div>
-		</article>
+<section class="sec">
+	<div class="container">
+		<header class="sec_header">
+			<h2 class="title">最新情報<span>NEWS</span></h2>
+		</header>
+		<div class="row">
+			<?php if (have_posts()) : ?>
+				<?php while (have_posts()) : the_post(); ?>
+					<div class="col-md-4">
+						<article id="post-<?= the_ID() ?>" <?= post_class('news'); ?>>
+							<div class="news_pic">
+								<a href="<?= the_permalink(); ?>">
+									<img src="<?= get_template_directory_uri(); ?>/assets/img/home/news_img-1.jpg" alt="">
+								</a>
+							</div>
+							<div class="news_meta">
+								<?= the_category(); ?>
+								<time class="news_time" datetime="<?= the_time('Y-m-d') ?>"><?= the_time('Y年m月d日') ?></time>
+							</div>
+							<h2 class="news_title"><a href="<?= the_permalink(); ?>"><?= the_title(); ?></a></h2>
+							<div class="news_desc">
+								<p><?= the_excerpt(); ?></p>
+								<p><a href="<?= the_permalink(); ?>">[続きを読む]</a></p>
+							</div>
+						</article>
+					</div>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
+
+		<p class="sec_btn">
+			<a href="" class="btn btn-default">最新情報の一覧<i class="fas fa-angle-right"></i></a>
+		</p>
+
 	</div>
-
-	<div class="col-md-4">
-		<article class="news">
-			<div class="news_pic">
-				<a href="#">
-					<img src="<?= get_template_directory_uri(); ?>/assets/img/home/news_img-2.jpg" alt="">
-				</a>
-			</div>
-			<div class="news_meta">
-				<ul class="post-categories">
-					<li><a href="#">お知らせ</a></li>
-				</ul>
-				<time class="news_time" datetime="2019-00-00">2019年00月00日</time>
-			</div>
-			<h2 class="news_title"><a href="#">タイトルタイトルタイトルタイトル</a></h2>
-			<div class="news_desc">
-				<p>概要が入ります。概要が入ります。概要が入ります。概要が入ります。概要が入ります。概要が入ります。</p>
-				<p><a href="#">[続きを読む]</a></p>
-			</div>
-		</article>
-	</div>
-
-	<div class="col-md-4">
-		<article class="news">
-			<div class="news_pic">
-				<a href="#">
-					<img src="<?= get_template_directory_uri(); ?>/assets/img/home/news_img-3.jpg" alt="">
-				</a>
-			</div>
-			<div class="news_meta">
-				<ul class="post-categories">
-					<li><a href="#">お知らせ</a></li>
-				</ul>
-				<time class="news_time" datetime="2019-00-00">2019年00月00日</time>
-			</div>
-			<h2 class="news_title"><a href="#">タイトルタイトルタイトルタイトル</a></h2>
-			<div class="news_desc">
-				<p>概要が入ります。概要が入ります。概要が入ります。概要が入ります。概要が入ります。概要が入ります。</p>
-				<p><a href="#">[続きを読む]</a></p>
-			</div>
-		</article>
-	</div>
-</div>
-
-<p class="sec_btn">
-	<a href="" class="btn btn-default">最新情報の一覧<i class="fas fa-angle-right"></i></a>
-</p>
-
-</div>
 </section>
 
 <section class="sec bg-gray">
