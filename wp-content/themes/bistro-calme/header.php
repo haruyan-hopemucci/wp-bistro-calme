d0610bf190b3c44f4de946ef63a2f8f4652d3ee7
haruyan-hopemucci <haruyanhpm@gmail.com>
2022-12-11 01:55:38 +0900

    feat: 検索ボックス作成

--- wp-content/themes/bistro-calme/header.php
+++ wp-content/themes/bistro-calme/header.php
@@ -29,8 +29,8 @@
                 <p><?= bloginfo('description'); ?></p>
             </div>
 
-            <form class="header_search">
-                <input type="text" placeholder="キーワードを入力">
+            <form action="<?= home_url('/') ?>" method="get" class="header_search">
+                <input type="text" placeholder="キーワードを入力" name="s" value="<?= the_search_query() ?>">
                 <i class="fas fa-search"></i>
             </form>
         </div>
