f4b90b9114ec4f3c2292c913dca99d39cf8cfab4
haruyan-hopemucci <haruyanhpm@gmail.com>
2022-12-11 00:43:57 +0900

    feat: template-parts loop-newsを作成

--- wp-content/themes/bistro-calme/front-page.php
+++ wp-content/themes/bistro-calme/front-page.php
@@ -9,26 +9,7 @@
 			<?php if (have_posts()) : ?>
 				<?php while (have_posts()) : the_post(); ?>
 					<div class="col-md-4">
-						<article id="post-<?= the_ID() ?>" <?= post_class('news'); ?>>
-							<div class="news_pic">
-								<a href="<?= the_permalink(); ?>">
-									<?php if (has_post_thumbnail()) : ?>
-										<?= the_post_thumbnail('medium'); ?>
-									<?php else : ?>
-										<img src="<?= get_template_directory_uri(); ?>/assets/img/common/noimage_600x400.png" alt="no image">
-									<?php endif; ?>
-								</a>
-							</div>
-							<div class="news_meta">
-								<?= the_category(); ?>
-								<time class="news_time" datetime="<?= the_time('Y-m-d') ?>"><?= the_time('Y年m月d日') ?></time>
-							</div>
-							<h2 class="news_title"><a href="<?= the_permalink(); ?>"><?= the_title(); ?></a></h2>
-							<div class="news_desc">
-								<p><?= the_excerpt(); ?></p>
-								<p><a href="<?= the_permalink(); ?>">[続きを読む]</a></p>
-							</div>
-						</article>
+						<?= get_template_part('template-parts/loop', 'news') ?>
 					</div>
 				<?php endwhile; ?>
 			<?php endif; ?>
---
+++ wp-content/themes/bistro-calme/index.php
@@ -0,0 +1,32 @@
+<?= get_header() ?>
+
+<h2 class="pageTitle">最新情報<span>NEWS</span></h2>
+
+<main class="main">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-9">
+                <h2 class="main_title"><?= wp_title(''); ?></h2>
+                <div class="row">
+
+                    <?php if (have_posts()) : ?>
+                        <?php while (have_posts()) : the_post(); ?>
+                            <div class="col-md-4">
+                                <?= get_template_part('template-parts/loop', 'news') ?>
+                            </div>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
+
+                </div>
+            </div>
+
+            <div class="col-12 col-md-3">
+                <?= get_sidebar('categories'); ?>
+                <?= get_sidebar('archives'); ?>
+            </div>
+        </div>
+    </div>
+</main>
+
+
+<?= get_footer() ?>
