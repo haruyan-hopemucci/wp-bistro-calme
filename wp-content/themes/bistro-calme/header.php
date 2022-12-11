7c9989e499c98334919fb4287f03dd01ab5d6c98
haruyan-hopemucci <haruyanhpm@gmail.com>
2022-12-12 02:38:42 +0900

    fix: bootstrapを導入
    読み込み順を最初にして既存スタイルを優先させる

--- wp-content/themes/bistro-calme/header.php
+++ wp-content/themes/bistro-calme/header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="<?= get_template_directory_uri(); ?>/assets/css/styles.min.css" rel="stylesheet">
     <link href="<?= get_template_directory_uri(); ?>/assets/css/custom-style.css" rel="stylesheet">
     <?php
