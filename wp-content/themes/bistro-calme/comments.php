976239b89635361a7b165c21bd7ddc307ebf803e
haruyan-hopemucci <haruyanhpm@gmail.com>
2022-12-17 13:20:05 +0900

    feat: コメントフォームタイトル変更

--- wp-content/themes/bistro-calme/comments.php
+++ wp-content/themes/bistro-calme/comments.php
@@ -1,14 +1,15 @@
 <section class="comments">
-<?php
-comment_form();
-if ( have_comments() ) :
-?>
-    <p><?php comments_number(); ?></p>
-    <ol class="commentlist">
-        <?php wp_list_comments(); ?>
-    </ol>
-<?php
-paginate_comments_links();
-endif;
-?>
+    <?php
+    $comment_form_params = array('title_reply' => 'コメント投稿フォーム');
+    comment_form($comment_form_params);
+    if (have_comments()) :
+    ?>
+        <p><?php comments_number(); ?></p>
+        <ol class="commentlist">
+            <?php wp_list_comments(); ?>
+        </ol>
+    <?php
+        paginate_comments_links();
+    endif;
+    ?>
 </section>
