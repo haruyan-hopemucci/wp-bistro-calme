21bf640ff76f2a6ae27812e60c65a32bbda59334
haruyan-hopemucci <haruyanhpm@gmail.com>
2022-12-19 18:59:55 +0900

    feat: その他のメニュー一覧表示

--- wp-content/themes/bistro-calme/single-menu.php
+++ wp-content/themes/bistro-calme/single-menu.php
@@ -4,6 +4,14 @@
 <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
 
+        <?php
+        // この記事のタクソノミー（料理の種類）を取得する
+        $obj = get_queried_object();
+        $this_id = $id;    // 後でその他のメニュー表示で使う
+        $kind_obj = get_the_terms($id, 'kind')[0];
+        $kind_name = $kind_obj->name;
+        $kind_slug = $kind_obj->slug;
+        ?>
         <main class="main">
             <section class="sec">
                 <div class="container">
@@ -64,82 +72,42 @@
             <section class="sec">
                 <div class="container">
                     <div class="sec_header">
-                        <h2 class="title title-jp">その他のフード</h2>
-                        <span class="title title-en">FOOD</span>
+                        <h2 class="title title-jp">その他の<?= $kind_name ?></h2>
+                        <span class="title title-en"><?= $kind_slug ?></span>
                     </div>
                     <div class="row justify-content-center">
 
-                        <div class="col-md-3">
-                            <section class="menu">
-                                <a href="#">
-                                    <figure class="menu_pic">
-                                        <img src="./assets/img/menu/menu-1@2x.jpg" alt="">
-                                    </figure>
-                                    <h3 class="menu_title">メニュー名が入ります</h3>
-                                    <p class="menu_price">800円</p>
-                                    <div class="menu_desc">
-                                        <p>
-                                            概要が入ります。概要が入ります。概要が入ります。概要が入ります。
-                                        </p>
-                                    </div>
-                                </a>
-                            </section>
-                        </div>
+                        <?php
+                        //メニューの投稿タイプを選択 1ページのアイテム数は4とする
+                        // ただし後から自身のページ表示を除くため１つ多く取得する
+                        $args = array('post_type' => 'menu', 'posts_per_page' => 5);
+                        // 絞り込みクエリの作成
+                        $taxquerysp = array('relation' => 'AND');
+                        $taxquerysp[] = array(
+                            'taxonomy' => 'kind',
+                            'terms' => $kind_slug,
+                            'field' => 'slug'
+                        );
+                        $args['tax_query'] = $taxquerysp;
 
-                        <div class="col-md-3">
-                            <section class="menu">
-                                <a href="#">
-                                    <figure class="menu_pic">
-                                        <img src="./assets/img/menu/menu-2@2x.jpg" alt="">
-                                    </figure>
-                                    <h3 class="menu_title">メニュー名が入ります</h3>
-                                    <p class="menu_price">800円</p>
-                                    <div class="menu_desc">
-                                        <p>
-                                            概要が入ります。概要が入ります。概要が入ります。概要が入ります。
-                                        </p>
-                                    </div>
-                                </a>
-                            </section>
-                        </div>
+                        $the_query = new WP_Query($args);
+                        ?>
 
-                        <div class="col-md-3">
-                            <section class="menu">
-                                <a href="#">
-                                    <figure class="menu_pic">
-                                        <img src="./assets/img/menu/menu-3@2x.jpg" alt="">
-                                    </figure>
-                                    <h3 class="menu_title">メニュー名が入ります</h3>
-                                    <p class="menu_price">800円</p>
-                                    <div class="menu_desc">
-                                        <p>
-                                            概要が入ります。概要が入ります。概要が入ります。概要が入ります。
-                                        </p>
-                                    </div>
-                                </a>
-                            </section>
-                        </div>
-
-                        <div class="col-md-3">
-                            <section class="menu">
-                                <a href="#">
-                                    <figure class="menu_pic">
-                                        <img src="./assets/img/menu/menu-4@2x.jpg" alt="">
-                                    </figure>
-                                    <h3 class="menu_title">メニュー名が入ります</h3>
-                                    <p class="menu_price">800円</p>
-                                    <div class="menu_desc">
-                                        <p>
-                                            概要が入ります。概要が入ります。概要が入ります。概要が入ります。
-                                        </p>
-                                    </div>
-                                </a>
-                            </section>
-                        </div>
+                        <?php if ($the_query->have_posts()) : ?>
+                            <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                                <?php
+                                // 記事自身のアイテムは読み飛ばす
+                                if ($this_id == $id) continue;
+                                ?>
+                                <div class="col-md-3">
+                                    <?= get_template_part('template-parts/loop', 'menu') ?>
+                                </div>
+                            <?php endwhile; ?>
+                        <?php endif; ?>
 
                         <div class="col-12">
                             <div class="sec_btn">
-                                <a href="" class="btn btn-default">メニュー一覧<i class="fas fa-angle-right"></i></a>
+                                <a href="<?= get_home_url(null, 'menu') ?>" class="btn btn-default">メニュー一覧<i class="fas fa-angle-right"></i></a>
                             </div>
                         </div>
 
